8e9d7f46ee8a0bd7456607d7c489c8c79cf24e46
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-15 18:56:08 +0530

    setting routes in middleware

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const http = require("http");
 const express=require('express');
 
 const app=express();
-app.use((req,res,next)=>{
-    console.log('This is a middleware')
-    next();
+
+app.use('/', (req,res,next)=>{
+    console.log('This will always run')
+   next()
 })
 
-app.use((req,res,next)=>{
+app.use('/product', (req,res,next)=>{
+    console.log('This is next middleware')
+    res.send("<h1>This is product Page</h1>")
+    
+})
+
+app.use('/', (req,res,next)=>{
     console.log('This is next middleware')
     res.send("<h1>Hello from Express</h1>")
     
 })
 
-// const server = http.createServer(app)
 
+
+// const server = http.createServer(app)
 // server.listen(3000);
 
 app.listen(3000);
