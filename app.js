2bfb94e3b7874090bdc2706e3cc1b0b354ebd2d8
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-16 19:59:31 +0530

    styling left

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
 const path=require('path')
 
+
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(adminRoutes);
