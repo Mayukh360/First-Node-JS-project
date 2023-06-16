29d5dd30ebd06a4bf3d6b2f3277644d99fd17fd3
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-16 20:30:04 +0530

    Form added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
+const contactRoute=require('./routes/contact')
+const successRoute=require('./routes/success')
 const path=require('path')
 
 
@@ -12,6 +14,8 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(adminRoutes);
 
 app.use(shopRoutes);
+app.use(contactRoute);
+app.use(successRoute)
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
