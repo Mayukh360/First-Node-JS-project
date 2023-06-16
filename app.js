7d3ddf85d1624929b48472a9a383acb2a04f1e6a
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-16 18:58:10 +0530

    Done

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const shopRoutes=require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/admin',adminRoutes);
+app.use(adminRoutes);
 
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>No Page Found</h1>')
-})
+// app.use((req,res,next)=>{
+//     res.status(404).send('<h1>No Page Found</h1>')
+// })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 
 router.get("/add-product", (req, res, next) => {
-  res.send('<form method="POST" action="/admin/product" ><label>Product</label><input type="text" name="title"/><label>Size</label><input type="text" name="size"/><button type="submit">Add Product</button></form>');
+  res.send('<form method="POST" action="/product" ><label>Product</label><input type="text" name="title"/><label>Size</label><input type="text" name="size"/><button type="submit">Add Product</button></form>');
 });
 
 router.post("/product", (req, res, next) => {
   console.log(req.body)
-  res.redirect("/shop");
+  res.redirect("/");
 });
 
 
